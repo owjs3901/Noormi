b43fbab1c34c444f1470a52b1d7932ea090d5d0b
bona0722 <sobona0722@gmail.com>
2020-08-23 21:16:44 +0900

    스플래쉬 아이콘, 글자 위치 조정

--- Noormi/Noormi/Splash.xaml.cs
+++ Noormi/Noormi/Splash.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-
+using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,12 +17,13 @@ namespace Noormi
         {
             InitializeComponent();
             
-            Task.Delay(3000)
-                .ContinueWith(t =>
-                    Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
-                        Application.Current.MainPage = new NavigationPage(new ListPage())
-                    )
-                );
+            // Task.Delay(3000)
+            //     .ContinueWith(t =>
+            //         Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+            //             Application.Current.MainPage = new NavigationPage(new ListPage())
+            //         )
+            //     );
         }
+
     }
 }
