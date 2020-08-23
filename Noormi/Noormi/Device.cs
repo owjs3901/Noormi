f4d968af5d8327368ac16dbf3ef9f828a3973db6
M <owjs3901@naver.com>
2020-08-23 21:29:38 +0900

    add Device Properties

--- Noormi/Noormi/Device.cs
+++ Noormi/Noormi/Device.cs
@@ -4,8 +4,11 @@ namespace Noormi
 {
     public class Device
     {
-        public string ProductName { get; }
-        public string Location { get; }
+        public string ProductName { get; } // 디바이스 이름
+        public string Location { get; } // 디바이스 위치
+        public string RegisterDate { get; } // 등록 일자
+        public string LastDate { get; } // 마지막 교체 일자
+        public string PredictionDate { get; } // 예상 교체 주기
 
         public Device(string productName, string location)
         {
