fbb78bc83723766f578007854f16ba61da2214f8
M <owjs3901@naver.com>
2020-08-23 22:13:00 +0900

    enable Arrow Button in ItemPage

--- Noormi/Noormi/ItemPage.xaml.cs
+++ Noormi/Noormi/ItemPage.xaml.cs
@@ -19,11 +19,25 @@ namespace Noormi
             
             var idx = FindByName("Index") as Label;
             var sz = FindByName("Size") as Label;
+            
+            SetArrow(index, size);
 
             idx.Text = (index + 1).ToString();
             sz.Text = size.ToString();
             
             BindingContext = device;
+        }
+
+        private void SetArrow(int index, int size)
+        {
+            var left = FindByName("Left") as ImageButton;
+            var right = FindByName("Right") as ImageButton;
+            
+            if(index + 1 != size)
+                right.Source = ImageSource.FromResource("Noormi.Images.enable.png");
+            if(index != 0)
+                left.Source = ImageSource.FromResource("Noormi.Images.enable.png");
+                
         }
     }
 }
