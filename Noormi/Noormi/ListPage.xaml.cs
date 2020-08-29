6342daf30764bd938985b89fb5200b59b053d0d8
M <owjs3901@naver.com>
2020-08-30 05:20:48 +0900

    add try catch

--- Noormi/Noormi/ListPage.xaml.cs
+++ Noormi/Noormi/ListPage.xaml.cs
@@ -23,24 +23,26 @@ namespace Noormi
             NavigationPage.SetHasNavigationBar(this, false);
             
             Devices = new List<Device>();
-
-            WebRequest request = WebRequest.Create("http://mbs-b.com:3000/api/devices/all");
-            using (Stream stream = request.GetResponse().GetResponseStream())
-            {
-                string str = new StreamReader(stream).ReadToEnd();
-                JObject jObject= JObject.Parse(str);
-                foreach (var obj in jObject.SelectToken("devices"))
-                    Devices.Add(JsonConvert.DeserializeObject<Device>(obj.ToString()));
-            }
             
-            if (Devices.Count == 0)
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://mbs-b.com:3000/api/devices/all");
+                request.Timeout = 3000;
+                using (Stream stream = request.GetResponse().GetResponseStream())
+                {
+                    string str = new StreamReader(stream).ReadToEnd();
+                    JObject jObject = JObject.Parse(str);
+                    foreach (var obj in jObject.SelectToken("devices"))
+                        Devices.Add(JsonConvert.DeserializeObject<Device>(obj.ToString()));
+                }
+            }
+            catch
             {
                 Devices.Add(new Device("PNAME", "LOC", 75,
                     "2020.02.30", "2020.02.30","D-20","2020.02.30"));
                 Devices.Add(new Device("PNAME1", "LOC2", 100,
                     "2020.02.30", "2020.02.30","D-20","2020.02.30"));
             }
-
             BindingContext = this;
         }
 
